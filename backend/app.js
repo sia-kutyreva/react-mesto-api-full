716206fe6741004e1b66f5f04687094fb774214f
Anastasiia Kutyreva <anastasia.kutyreva2528@icloud.com>
2021-04-16 18:11:27 +0300

    15-й проект, 2-я итерация

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,13 @@ const { requestLogger, errorLogger } = require('./middlewere/logger');
 const app = express();
 const { PORT = 3000 } = process.env;
 
-app.use(cors());
+app.options('*', cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  next();
+});
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
