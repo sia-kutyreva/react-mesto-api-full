f37ce2f2ee0c8f24a93168ce5944e56aa0c62362
Anastasiia Kutyreva <anastasia.kutyreva2528@icloud.com>
2021-04-19 15:17:40 +0300

    15-й проект, 2-я итерация

--- backend/app.js
+++ backend/app.js
@@ -18,13 +18,13 @@ const { requestLogger, errorLogger } = require('./middlewere/logger');
 const app = express();
 const { PORT = 3000 } = process.env;
 
-const corsOptions = {
-  origin: 'https://mesto.master.nomoredomains.icu',
-  credentials: true,
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  allowedHeaders: ['Origin', 'Content-Type', 'Authorization'],
-  optionsSuccessStatus: 204,
-};
+app.options('*', cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  next();
+});
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -32,8 +32,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
-
-app.use('*', cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
