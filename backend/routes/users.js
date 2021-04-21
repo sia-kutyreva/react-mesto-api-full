4a5e8cc734d911baff6d78f16f12afa93d0ae6e9
Anastasiia Kutyreva <anastasia.kutyreva2528@icloud.com>
2021-04-21 15:37:00 +0300

    15-й проект, 3-я итерация

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,6 +13,7 @@ usersRouter.get('/users', getUsers);
 usersRouter.get('/users/me', getCurrentUser);
 
 usersRouter.get('/users/:userId', celebrate({
+  headers: Joi.object().keys({}).unknown(true),
   params: Joi.object().keys({
     userId: Joi.string().length(24).hex(),
   }),
@@ -26,6 +27,7 @@ usersRouter.patch('/users/me', celebrate({
 }), updateUserProfile);
 
 usersRouter.patch('/users/me/avatar', celebrate({
+  headers: Joi.object().keys({}).unknown(true),
   body: Joi.object().keys({
     avatar: Joi.string().required().regex(/^((http|https):\/\/)(www\.)?([\w\W\d]{1,})(\.)([a-zA-Z]{1,10})([\w\W\d]{1,})?$/),
   }),
