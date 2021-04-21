55f33168ab3f1311e6bf57cf65058d1634a66fcc
Anastasiia Kutyreva <anastasia.kutyreva2528@icloud.com>
2021-04-21 14:46:05 +0300

    15-й проект, 3-я итерация

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -24,8 +24,7 @@ const createUser = (req, res, next) => {
           password: hash,
         })
           .then((data) => {
-            const token = jwt.sign({ _id: data._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-            res.status(200).send({ email: data.email, token });
+            res.status(200).send({ email: data.email });
           })
           .catch(next));
     })
